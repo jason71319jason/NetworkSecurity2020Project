96181ef9330eca4c905693960c018c73aa001246
JasonHsieh <jason71319jason@gmail.com>
2020-06-02 22:19:27 +0800

    Functionalize test.py

--- test.py
+++ test.py
@@ -7,53 +7,47 @@ try:
 except ImportError:
 	import xml.etree.ElementTree as ET
 
+def show_eventID(xmlTree, log_type):
+    root = xmlTree.getroot()
+    for elem in root:
+            xmlns = elem.tag.split("}")[0] + "}"
+    for eventId in root.iter(xmlns+"EventID"):
+            print("{} EventID: {}".format(log_type, eventId.text))
+
+def load_xml(log, full_path):
+    if log == "Security.xml":
+            show_eventID(ET.ElementTree(file=full_path), "Security")
+    elif log == "Sysmon.xml":
+            show_eventID(ET.ElementTree(file=full_path), "Sysmon")
+    else:
+            print("Unknown xml file name...")
+
+def load_json(log, full_path):
+    if log == "Wireshark.json":
+            for elem in json.load(open(full_path, encoding="latin-1")):
+                    source = elem["_source"]
+                    layers = source["layers"]
+                    frame = layers["frame"]
+                    print("Wireshark frame.time: "+frame["frame.time"])
+    else:
+            print("Unknown json file name...")
+
+def file_check(log, full_path):
+    if log.endswith("xml"):
+            load_xml(log, full_path)
+    elif log.endswith("json"):
+            load_json(log, full_path)
+    else:
+            print("Only for xml or json...")
 
 if __name__ == "__main__":
 
 	parser = ArgumentParser()
-	parser.add_argument("filePath", help="root path of data")
+	parser.add_argument("file_path", help="root path of data")
 	args = parser.parse_args()
-	
-	folders = listdir(args.filePath)
-	num = 1
-	for f in folders:
-		dataPath = join(args.filePath, f)
-		print("testcase %d: %s" % (num, f))
-		num += 1
-		data = listdir(dataPath)
-		for d in data:
-			fullPath = join(dataPath, d)
-			fileCheck = d.split(".")
-			if fileCheck[len(fileCheck)-1] == "xml":
-				#print(d+" : xml")
-				if fileCheck[0] == "Security":
-					xmlTree = ET.ElementTree(file=fullPath)
-					root = xmlTree.getroot()
-					for elem in root:
-						xmlns = elem.tag.split("}")[0] + "}"
-					for eventId in root.iter(xmlns+"EventID"):
-						print("Security EventID: "+eventId.text)
-				elif fileCheck[0] == "Sysmon":
-					xmlTree = ET.ElementTree(file=fullPath)
-					root = xmlTree.getroot()
-					for elem in root:
-						xmlns = elem.tag.split("}")[0] + "}"
-					for eventId in root.iter(xmlns+"EventID"):
-						print("Sysmon EventID: "+eventId.text)
-				else:
-					print("Unknown xml file name...")
-			elif fileCheck[len(fileCheck)-1] == "json":
-				#print(d+" : json")
-				if fileCheck[0] == "Wireshark":
-					inputFile = open(fullPath, encoding="latin-1")
-					jsonData = json.load(inputFile)
-					for elem in jsonData:
-						source = elem["_source"]
-						layers = source["layers"]
-						frame = layers["frame"]
-						print("Wireshark frame.time: "+frame["frame.time"])
-				else:
-					print("Unknown json file name...")
-			else:
-				print("Only for xml or json...")
-			#print(d)
+
+	for num, testcase in enumerate(listdir(args.file_path)):
+		print("testcase {}: {}".format(num+1, testcase))
+		for log in listdir(join(args.file_path, testcase)):
+                    file_check(log, join(args.file_path, testcase, log))
+
